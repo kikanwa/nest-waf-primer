53075bc2dd85f75c236fc6680deb5ca5fc919be2
Kikanwa Ohanugo <kikanwa.ohanugo@pau.edu.ng>
2022-02-21 17:07:59 +0100

    First Commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('hello2')
+  @Render('index.html') getHello2(): {} {
+    return this.appService.getHello2();
+  }
+
+  @Get('home') 
+  @Render('home.html') 
+  getHome(): {} {
+    return this.appService.getHome();
+  }
+  @Get('about-us')
+  @Render('about-us.html') 
+  getAboutUs(): {} {
+    return this.appService.getAboutUs();
+  }
 }
+function getAboutUs(): any {
+  throw new Error('Function not implemented.');
+}
+
